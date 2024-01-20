ab10bf59a4f1fd001fcbd625c33d20054370753b
lsherman2 <logansherman1@gmail.com>
2024-01-20 10:19:51 -0600

    Added function to add up the calories

--- script.js
+++ script.js
@@ -32,4 +32,23 @@ function addEntry() {
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
 
+
+
+function getCaloriesFromInputs(list) {
+  let calories = 0;
+
+  for (let i = 0; i < list.length; i++) {
+    const currVal = cleanInputString(list[i].value);
+    const invalidInputMatch = isInvalidInput(currVal);
+
+    if (invalidInputMatch) {
+      alert(`Invalid Input: ${invalidInputMatch[0]}`);
+      isError = true;
+      return null;
+    }
+    calories += Number(currVal);
+  }
+  return calories;
+}
+
 addEntryButton.addEventListener("click", addEntry);
